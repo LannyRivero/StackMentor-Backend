44fd970d07259b63395bc8d3b98b27a5b50ce376
PFBregon <bregon@gmail.com>
2025-03-10 13:53:08 +0100

    first commit

---
+++ src/main/java/dev/patriciafb/stackmentor/StackmentorApplication.java
@@ -0,0 +1,13 @@
+package dev.patriciafb.stackmentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackmentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackmentorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/patriciafb/stackmentor/StackmentorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patriciafb.stackmentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackmentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
