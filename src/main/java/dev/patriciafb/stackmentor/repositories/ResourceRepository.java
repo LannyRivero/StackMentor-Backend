e426247624950d718c050a4f5a89a23f26c96cfd
PFBregon <bregon@gmail.com>
2025-03-11 15:02:42 +0100

    services done

--- src/main/java/dev/patriciafb/stackmentor/repositories/ResourceRepository.java
+++ src/main/java/dev/patriciafb/stackmentor/repositories/ResourceRepository.java
@@ -1,6 +1,8 @@
 package dev.patriciafb.stackmentor.repositories;
 
 import dev.patriciafb.stackmentor.models.Resource;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
     List<Resource> findByCategory(String category);
--- src/main/java/dev/patriciafb/stackmentor/services/ResourceService.java
+++ src/main/java/dev/patriciafb/stackmentor/services/ResourceService.java
@@ -1,5 +1,21 @@
 package dev.patriciafb.stackmentor.services;
 
-public class ResourceService {
+import dev.patriciafb.stackmentor.models.Resource;
+import dev.patriciafb.stackmentor.repositories.ResourceRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
+@Service
+public class ResourceService {
+    
+    private ResourceRepository resourceRepository;
+    public List<Resource> getAllResources() {
+        return resourceRepository.findAll();
+    }
+    public Resource saveResource(Resource resource) {
+        return resourceRepository.save(resource);
+    }
+    public List<Resource> getResourcesByCategory(String category) {
+        return resourceRepository.findByCategory(category);
+    }
 }
