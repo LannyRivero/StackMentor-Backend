a3e71335736dce4e8299b443f8d866f9674b3985
PFBregon <bregon@gmail.com>
2025-03-11 13:02:07 +0100

    models done

--- src/main/java/dev/patriciafb/stackmentor/models/Comment.java
+++ src/main/java/dev/patriciafb/stackmentor/models/Comment.java
@@ -1,5 +1,56 @@
 package dev.patriciafb.stackmentor.models;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+@Entity
 public class Comment {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    @ManyToOne
+    @JoinColumn(name = "resource_id")
+    private Resource resource;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    public Comment() {}
+    public Comment(String content, Resource resource, User user) {
+        this.content = content;
+        this.resource = resource;
+        this.user = user;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public Resource getResource() {
+        return resource;
+    }
+    public void setResource(Resource resource) {
+        this.resource = resource;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/dev/patriciafb/stackmentor/models/Resource.java
+++ src/main/java/dev/patriciafb/stackmentor/models/Resource.java
@@ -1,5 +1,72 @@
 package dev.patriciafb.stackmentor.models;
 
-public class Resource {
+import jakarta.persistence.*;
+import java.util.List;
 
+@Entity
+public class Resource {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String category; // Ej: FRONTEND, BACKEND, TESTING
+    private String type; // Ej: LINK, FILE
+    private String url;
+    @ManyToOne
+    @JoinColumn(name = "uploader_id")
+    private User uploader;
+    @OneToMany(mappedBy = "resource", cascade = CascadeType.ALL)
+    private List<Comment> comments;
+ 
+    public Resource() {}
+    public Resource(String title, String category, String type, String url, User uploader) {
+        this.title = title;
+        this.category = category;
+        this.type = type;
+        this.url = url;
+        this.uploader = uploader;
+    }
+   
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public User getUploader() {
+        return uploader;
+    }
+    public void setUploader(User uploader) {
+        this.uploader = uploader;
+    }
+    public List<Comment> getComments() {
+        return comments;
+    }
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
 }
